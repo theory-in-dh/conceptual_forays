ebca2d07706ef8d850cfed8862fadfe8e18c6d67
Silvia Gutiérrez <silviaegt@gmail.com>
2021-11-29 19:28:30 +0100

    :heavy_plus_sign:
    added some options to explore the data

--- explore_theorists.R
+++ explore_theorists.R
@@ -1,3 +1,18 @@
+#load packages
 library(tidyverse)
 library(readr)
+library(janitor)
+
+# fetch data
 data <- read_csv("data_reconciliation/theory_dictionary_wikidata_extended.csv")
+#normalize column names
+data <- clean_names(data)
+data
+#explore distinct genders 
+data %>% 
+  distinct(wikidata_id, .keep_all = TRUE) %>% # eliminates duplicates (! remember: there are some persons that appear more than once)
+  count(sex_or_gender)
+
+data %>% 
+  distinct(wikidata_id, .keep_all = TRUE) %>%
+  count(country_of_citizenship, sort = T)
