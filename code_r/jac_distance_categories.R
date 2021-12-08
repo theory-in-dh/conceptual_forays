eef3f33facf176f033646554a00bd54f29323d34
Silvia Gutiérrez <silviaegt@gmail.com>
2021-12-08 14:53:50 +0100

    :art: updated code :gift: new dataset

--- code_r/jac_distance_categories.R
+++ code_r/jac_distance_categories.R
@@ -1,10 +1,10 @@
 library(tidyverse)
 library(readr)
-library(RecordLinkage)
 #https://journal.r-project.org/archive/2014-1/loo.pdf
-install.packages("stringdist")
+#install.packages("stringdist")
 library(stringdist)
-install.packages("wordcloud")
+#install.packages("wordcloud")
+library(wordcloud)
 
 categories_wikipedia <- read_csv("data_api_queries/categories_wikipedia.csv")
 categories_wikipedia <- categories_wikipedia %>% 
@@ -38,10 +38,9 @@ wordcloud(words = cat_count_words$words, freq = cat_count_words$n, min.freq = 30
 
 
 df <- categories_wikipedia_distances %>% 
-  filter(jac < 1,
+  filter(jac < 0.6,
          !grepl('WikiProject|Wikipedia|[C|c]onspiracy|Christ|[M|m]ilitary|articles|journals|missing|Satanic|[T|t]errorism|abuse|backlog|Lists|albums', category),
          !grepl('[C|c]onspiracy|[T|t]elevision|Nazis', snippet))
-
 
 df_words_count <- df %>%
   select(category) %>% 
@@ -49,9 +48,20 @@ df_words_count <- df %>%
   anti_join(swen) %>% 
   count(words)
 
-
 wordcloud(words = df_words_count$words, freq = df_words_count$n, min.freq = 1,
           max.words=100, random.order=FALSE, rot.per=0.35, 
           colors=brewer.pal(8, "Dark2"))
 
 write_csv(df, "data_api_queries/wikicategories_distances_filtered.csv")
+
+humans <- read_csv("data_api_queries/theory_categories_humans.csv")
+
+humans_jac06 <- left_join(df, humans, by = c("title" = "category"))
+
+humans_jac06 <- humans_jac06 %>% 
+  select(-...1,-ns) %>% 
+  rename(category_title = title,
+         wikidata_url = item,
+         wikidata_entity = itemLabel)
+
+write_csv(humans_jac06, "data_api_queries/theory_categories_humans_smalljacdistance.csv")
