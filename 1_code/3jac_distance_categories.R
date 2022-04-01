eb379aa9fcf1c4a3d3770578837cac3ce13ca6ac
Silvia Gutiérrez <silviaegt@gmail.com>
2022-04-01 12:49:30 +0200

    :gift: updated code with headers

--- 1_code/3jac_distance_categories.R
+++ 1_code/3jac_distance_categories.R
@@ -1,20 +1,22 @@
+#### Upload packages ####
 library(tidyverse)
 library(readr)
-#https://journal.r-project.org/archive/2014-1/loo.pdf
-#install.packages("stringdist")
 library(stringdist)
-#install.packages("wordcloud")
-library(wordcloud)
-#theoriesof <- read_csv("data/theoriesof_complete.csv") 
-categories_wikipedia <- read_csv("data_api_queries/complete_categories_wikipedia.csv")
-names(theoriesof)
-length(unique(categories_wikipedia$title))
-length(unique(categories_wikipedia$query_string))
 
-title_count <- categories_wikipedia %>% 
-  count(title)
-esquisse::esquisser(category_count)
-ggplot(category_count) +
+#### Read categories retrieved from matching with "theory of" strings ####
+
+categories_wikipedia <- read_csv("3_data_api_queries/complete_categories_wikipedia.csv")
+names(categories_wikipedia)
+length(unique(categories_wikipedia$title)) #1266
+length(unique(categories_wikipedia$query_string)) #1529
+
+#### Count number of categories by each "theory of" string ####
+catcountbystring<- categories_wikipedia %>%
+  group_by(query_string) %>%
+  summarise(count = n_distinct(title))
+
+#### Create histogram of number of categories by string query (mf cont: 10 categories per query) ####
+ggplot(catcountbystring) +
   aes(x = count) +
   geom_histogram(bins = 30L, fill = "#112446") +
   labs(
@@ -23,46 +25,53 @@ ggplot(category_count) +
   ) +
   theme_minimal()
 
-category_count <- categories_wikipedia %>%
-  group_by(query_string) %>%
-  summarise(count = n_distinct(title))
 
+#### Delete "Category:" string to match and compare with query string ####
 categories_wikipedia <- categories_wikipedia %>% 
   mutate(category = stringr::str_replace_all(categories_wikipedia$title, "Category:", ""))
+
+#### Calculate jac distance ####
 categories_wikipedia_distances <- categories_wikipedia %>% 
   mutate(jac = purrr::map2(.x= categories_wikipedia$category, .y= categories_wikipedia$query_string,  ~  stringdist(.x, .y, method = "jaccard", q=3)),
          lev = purrr::map2(.x= categories_wikipedia$category, .y= categories_wikipedia$query_string,  ~  stringdist(.x, .y, method = "lv")))
+
 categories_wikipedia_distances <- categories_wikipedia_distances %>% 
   mutate(jac = unlist(jac),
          lev = unlist(lev))
 
-
-#### Categories wordcloud
+cat_count <- categories_wikipedia_distances %>% 
+  count(category)
+#### Data exploration #### 
+##### Categories wordcloud ##### 
 library(wordcloud)
 set.seed(1234)
 wordcloud(words = cat_count$category, freq = cat_count$n, min.freq = 5,
           max.words=200, random.order=FALSE, rot.per=0.35, 
           colors=brewer.pal(8, "Dark2"))
 
-#### Words wordcloud
+##### Terms wordcloud ##### 
 swen <- tibble(words = stopwords::stopwords("en", source = "snowball"))
-
 cat_count_words <- cat_count %>% 
   tidytext::unnest_tokens(words, category) %>% 
   anti_join(swen)
-
 wordcloud(words = cat_count_words$words, freq = cat_count_words$n, min.freq = 30,
           max.words=100, random.order=FALSE, rot.per=0.35, 
           colors=brewer.pal(4, "Dark2"))
 
+#### Filter categories #### 
+
+##### Filter by jac dist ##### 
 df <- categories_wikipedia_distances %>% 
   filter(jac < 0.6)
 
-length(unique(df$query_string))
+##### Filter by strange kw found in wordcloud ##### 
 df <- categories_wikipedia_distances %>% 
   filter(jac < 0.6,
          !grepl('WikiProject|Wikipedia|[C|c]onspiracy|Christ|[M|m]ilitary|articles|journals|missing|Satanic|[T|t]errorism|abuse|backlog|Lists|albums', category),
          !grepl('[C|c]onspiracy|[T|t]elevision|Nazis', snippet))
+
+
+#### Explore results with wordcloud #### 
 
 df_words_count <- df %>%
   select(category) %>% 
@@ -70,13 +79,14 @@ df_words_count <- df %>%
   anti_join(swen) %>% 
   count(words)
 
-wordcloud(words = df_words_count$words, freq = df_words_count$n, min.freq = 1,
+wordcloud(words = df_words_count$words, freq = df_words_count$n, scale =c(4,1), min.freq = 1,
           max.words=100, random.order=FALSE, rot.per=0.35, 
           colors=brewer.pal(8, "Dark2"))
 
-write_csv(df, "data_api_queries/wikicategories_distances_filtered.csv")
 
-humans <- read_csv("data_api_queries/theory_categories_humans.csv")
+write_csv(df, "3_data_api_queries/wikicategories_distances_filtered.csv")
+
+humans <- read_csv("4_data_reconciliation/theorystrings_categories_humans.csv")
 
 humans_jac06 <- left_join(df, humans, by = c("title" = "category"))
 
@@ -86,4 +96,4 @@ humans_jac06 <- humans_jac06 %>%
          wikidata_url = item,
          wikidata_entity = itemLabel)
 
-write_csv(humans_jac06, "data_api_queries/theory_categories_humans_smalljacdistance.csv")
+write_csv(humans_jac06, "5_data_enrichment/theorystrings_humans_smalljacdistance.csv")
