80566f6528321c9a96f99350d1a858f925933d75
silviaegt <silviaegt@gmail.com>
2022-04-02 18:47:02 +0200

    :pencil2 commented code

--- 1_code/2_query_wikipedia.py
+++ 1_code/2_query_wikipedia.py
@@ -1,14 +1,22 @@
+#### Import libraries
 import requests
 import pandas as pd
 import os
 
-string_filename = "data/theoriesof_complete.csv"
+#### Get file with all "theory of strings"
+string_filename = "1_data/1_theoriesof_complete.csv"
+
+#### Get file with all "theory of strings" and read it with pandas
 df = pd.read_csv(string_filename)
 df
-#df_selection = df[df.freq >= 2]
+
+#### Get all strings and read them as a list
 query_strings = df["normalized_string"].values.tolist()
-query_strings
+
+#### Create empty dataframe to save query results
 all_results_df = pd.DataFrame()
+
+#### Run query and save results in df
 for query_string in query_strings:
     url = f"https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch={query_string}&srnamespace=14"
     #url = f"https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=intitle:{query_string}&srnamespace=14"
@@ -21,5 +29,9 @@ for query_string in query_strings:
         print(result)
 
 print(all_results_df)
+
+#### Create column "title" with category name without "Category:" string
 all_results_df["category"] = all_results_df["title"].str.split("Category:").apply(lambda l: l[1])
+
+#### Save results in csv
 all_results_df.to_csv("1_data/3_complete_categories_wikipedia.csv")
