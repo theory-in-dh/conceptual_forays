ab4ed6042087b53d24169017def9c543df749e1f
silviaegt <silviaegt@gmail.com>
2022-04-02 19:48:26 +0200

    :pencil2: comments added

--- 1_code/5explore_theorists.R
+++ 1_code/5explore_theorists.R
@@ -1,14 +1,14 @@
-#load packages
+##### Load libraries #####
 library(tidyverse)
 library(readr)
 library(janitor)
 
-# fetch data
+##### Fetch data #####
 data <- read_csv("1_data/4_theory_dictionary_wikidata_extended.csv")
-#normalize column names
+##### Clean column names #####
 data <- clean_names(data)
 data
-#explore distinct genders 
+##### Explore distinct genders #####
 data %>% 
   distinct(wikidata_id, .keep_all = TRUE) %>% # eliminates duplicates (! remember: there are some persons that appear more than once)
   count(sex_or_gender)
