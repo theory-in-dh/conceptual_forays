e946301e85fef328fbe4eca99edad7b4d7fcc5e7
Silvia Gutiérrez <silviaegt@gmail.com>
2023-02-26 20:31:41 -0600

    :gift:

--- code/5explore_theorists.R
+++ code/5explore_theorists.R
@@ -4,15 +4,19 @@ library(readr)
 library(janitor)
 
 ##### Fetch data #####
-data <- read_csv("1_data/4_theory_dictionary_wikidata_extended.csv")
-##### Clean column names #####
-data <- clean_names(data)
-data
-##### Explore distinct genders #####
-data %>% 
-  distinct(wikidata_id, .keep_all = TRUE) %>% # eliminates duplicates (! remember: there are some persons that appear more than once)
-  count(sex_or_gender)
+full_theorists <- read_csv("https://raw.githubusercontent.com/theory-in-dh/conceptual_forays/main/data/4_theorystrings_humans_extended_withcategories.csv")
+found_theorists <- read_csv("https://raw.githubusercontent.com/theory-in-dh/conceptual_forays/main/data/reference.theorists.full.csv")
 
-data %>% 
-  distinct(wikidata_id, .keep_all = TRUE) %>%
-  count(country_of_citizenship, sort = T)
+##### Clean column names #####
+full_theorists <- clean_names(full_theorists)
+found_theorists <- clean_names(found_theorists)
+theorists <- left_join(found_theorists, full_theorists, by = c("keyword" = "item_label"))
+
+##### Explore distinct genders #####
+theorists %>% 
+  distinct(q_id, .keep_all = TRUE) %>% # eliminates duplicates (! remember: there are some persons that appear more than once)
+  count(sex_gender, sort = T)
+
+theorists %>% 
+  distinct(q_id, .keep_all = TRUE) %>%
+  count(country_citizenship, sort = T)
